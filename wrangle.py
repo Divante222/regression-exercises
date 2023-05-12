631cbf2c7619eb4760b9f21387d51a885420859d
Divante Parness <divante111@gmail.com>
2023-05-12 17:10:09 -0500

    finished the days work

--- wrangle.py
+++ wrangle.py
@@ -13,6 +13,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SelectKBest, RFE, f_regression, SequentialFeatureSelector
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LassoLars
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import TweedieRegressor
 
 def new_zillow_data(SQL_query, url):
     '''
@@ -65,6 +70,10 @@ def preparing_data_zillow(df):
     droping unwanted rows for zillow first exercise
     converting float columns to integers
     '''
+    df = df.drop(columns = ['Unnamed: 0', 'propertylandusedesc'])
+    df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet[df.calculatedfinishedsquarefeet < 25000]
+    
+    df = df[df.taxvaluedollarcnt < df.taxvaluedollarcnt.quantile(.95)].copy()
     df = df.dropna()
     df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet.astype(int).copy()
     df.fips = df.fips.astype(int).copy()
@@ -156,3 +165,95 @@ def select_kbest(X_train, y_train, the_k):
     kbest.fit(X_train, y_train)
     return X_train.columns[kbest.get_support()]
 
+def metrics_reg(y, yhat):
+    '''
+    send in y_true, y_pred and returns rmse, r2
+    '''
+    rmse = mean_squared_error(y, yhat, squared=False)
+    r2 = r2_score(y, yhat)
+    return rmse, r2
+
+def get_X_train_val_test(train,validate, test, x_target, y_target):
+    '''
+    geting the X's and y's and returns them
+    '''
+    X_train = train.drop(columns = x_target)
+    X_validate = validate.drop(columns = x_target)
+    X_test = test.drop(columns = x_target)
+    y_train = train[y_target]
+    y_validate = validate[y_target]
+    y_test = test[y_target]
+
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
+
+
+def get_model_numbers(X_train, X_validate, X_test, y_train, y_validate, y_test):
+    baseline = y_train.mean()
+    baseline_array = np.repeat(baseline, len(X_train))
+    rmse, r2 = metrics_reg(y_train, baseline_array)
+
+    metrics_train_df = pd.DataFrame(data=[
+    {
+        'model_train':'baseline',
+        'rmse':rmse,
+        'r2':r2
+    }
+    ])
+    metrics_validate_df = pd.DataFrame(data=[
+    {
+        'model_validate':'baseline',
+        'rmse':rmse,
+        'r2':r2
+    }
+    ])
+
+
+    Linear_regression1 = LinearRegression()
+    Linear_regression1.fit(X_train,y_train)
+    predict_linear = Linear_regression1.predict(X_train)
+    rmse, r2 = metrics_reg(y_train, predict_linear)
+    metrics_train_df.loc[1] = ['ordinary least squared(OLS)', rmse, r2]
+
+    predict_linear = Linear_regression1.predict(X_validate)
+    rmse, r2 = metrics_reg(y_validate, predict_linear)
+    metrics_validate_df.loc[1] = ['ordinary least squared(OLS)', rmse, r2]
+
+
+    lars = LassoLars()
+    lars.fit(X_train, y_train)
+    pred_lars = lars.predict(X_train)
+    rmse, r2 = metrics_reg(y_train, pred_lars)
+    metrics_train_df.loc[2] = ['lasso lars(lars)', rmse, r2]
+
+    pred_lars = lars.predict(X_validate)
+    rmse, r2 = metrics_reg(y_validate, pred_lars)
+    metrics_validate_df.loc[2] = ['lasso lars(lars)', rmse, r2]
+
+
+    pf = PolynomialFeatures(degree=2)
+    X_train_degree2 = pf.fit_transform(X_train)
+   
+
+    pr = LinearRegression()
+    pr.fit(X_train_degree2, y_train)
+    pred_pr = pr.predict(X_train_degree2)
+    rmse, r2 = metrics_reg(y_train, pred_pr)
+    metrics_train_df.loc[3] = ['Polynomial Regression(poly2)', rmse, r2]
+
+    X_validate_degree2 = pf.transform(X_validate)
+    pred_pr = pr.predict(X_validate_degree2)
+    rmse, r2 = metrics_reg(y_validate, pred_pr)
+    metrics_validate_df.loc[3] = ['Polynomial Regression(poly2)', rmse, r2]
+
+
+    glm = TweedieRegressor(power=1, alpha=0)
+    glm.fit(X_train, y_train)
+    pred_glm = glm.predict(X_train)
+    rmse, r2 = metrics_reg(y_train, pred_glm)
+    metrics_train_df.loc[4] = ['Generalized Linear Model (GLM)', rmse, r2]
+
+    pred_glm = glm.predict(X_validate)
+    rmse, r2 = metrics_reg(y_validate, pred_glm)
+    metrics_validate_df.loc[4] = ['Generalized Linear Model (GLM)', rmse, r2]
+
+    return metrics_train_df, metrics_validate_df
